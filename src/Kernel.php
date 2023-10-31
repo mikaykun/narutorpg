dd389d143461b41decb472011bbaf7b26c4c33b9
Joshua Schmidtke <coding@mikaykun.dev>
2023-10-31 08:42:24 +0100

    Add basic Symfony setup

---
+++ src/Kernel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace NarutoRPG;
+
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+
+class Kernel extends BaseKernel
+{
+    use MicroKernelTrait;
+}
