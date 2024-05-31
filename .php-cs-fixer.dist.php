2296974123b55190ecc7a637d0bccfc8592e29a2
Joshua Schmidtke <coding@mikaykun.dev>
2024-05-31 21:52:59 +0200

    Update PHP Coding Standards Fixer config

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -11,7 +11,8 @@ $finder = (new PhpCsFixer\Finder())
 return (new PhpCsFixer\Config())
     ->setRiskyAllowed(true)
     ->setRules([
-        '@PSR12' => true,
+        '@PER-CS' => true,
+        '@PHP81Migration' => true,
         'array_push' => true,
         'array_syntax' => ['syntax' => 'short'],
         'cast_spaces' => ['space' => 'none'],
@@ -23,11 +24,20 @@ return (new PhpCsFixer\Config())
         'native_function_casing' => true,
         'no_alias_functions' => true,
         'no_blank_lines_after_phpdoc' => true,
+        'no_trailing_comma_in_singleline' => true,
+        'no_unneeded_control_parentheses' => true,
         'no_unused_imports' => true,
+        'no_useless_else' => true,
+        'ordered_class_elements' => true,
         'ordered_imports' => ['imports_order' => ['class', 'function', 'const'], 'sort_algorithm' => 'alpha'],
+        'phpdoc_no_access' => true,
         'phpdoc_no_empty_return' => true,
         'phpdoc_no_package' => true,
+        'phpdoc_scalar' => true,
         'phpdoc_trim' => true,
+        'phpdoc_types' => true,
+        'phpdoc_types_order' => ['null_adjustment' => 'always_last', 'sort_algorithm' => 'none'],
+        'single_line_throw' => false,
         'trailing_comma_in_multiline' => ['elements' => ['arrays']],
     ])
     ->setFinder($finder)
